7bb5bc69af54d555d974be52c382ecd75292a750
LukystarWar <lukystar00@gmail.com>
2020-11-29 19:09:45 -0500

    View Tabloide

---
+++ front/tabloide.php
@@ -0,0 +1,236 @@
+<!DOCTYPE html>
+<html lang="pt-br" dir="ltr">
+<head>
+  <meta charset="utf-8">
+  <title>TABLOIDE</title>
+  <link rel="stylesheet" href="assets/css/bootstrap.css">
+  <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+  <link rel="stylesheet" href="assets/css/tabloide.css">
+</head>
+<body>
+  <div class="container">
+    <!-- LINHA -->
+    <div class="row">
+
+          <div class="col-sm" id="item" >
+            <img src="../img/por.png" alt="" id="item2">
+            <h6>Achocolatado Nestlé Nescau Top Mesmo</h6>
+            <hr>
+            <img src="../img/nescau.png" alt="" id="logo"  >
+            <h4 id="por">R$ 4,50</h4>
+          </div>
+
+          <div class="col-sm" id="item" >
+            <img src="../img/por.png" alt="" id="item2">
+            <h6>Achocolatado Nestlé Nescau Top Mesmo</h6>
+            <hr>
+            <img src="../img/nescau.png" alt="" id="logo"  >
+            <h4 id="por">R$ 4,50</h4>
+          </div>
+
+          <div class="col-sm" id="item" >
+            <img src="../img/por.png" alt="" id="item2">
+            <h6>Achocolatado Nestlé Nescau Top Mesmo</h6>
+            <hr>
+            <img src="../img/nescau.png" alt="" id="logo"  >
+            <h4 id="por">R$ 4,50</h4>
+          </div>
+
+          <div class="col-sm" id="item" >
+            <img src="../img/por.png" alt="" id="item2">
+            <h6>Achocolatado Nestlé Nescau Top Mesmo</h6>
+            <hr>
+            <img src="../img/nescau.png" alt="" id="logo"  >
+            <h4 id="por">R$ 4,50</h4>
+          </div>
+
+          <div class="col-sm" id="item" >
+            <img src="../img/por.png" alt="" id="item2">
+            <h6>Achocolatado Nestlé Nescau Top Mesmo</h6>
+            <hr>
+            <img src="../img/nescau.png" alt="" id="logo"  >
+            <h4 id="por">R$ 4,50</h4>
+          </div>
+
+    </div>
+    <!-- linha -->
+    <div class="row">
+
+          <div class="col-sm" id="item" >
+            <img src="../img/por.png" alt="" id="item2">
+            <h6>Achocolatado Nestlé Nescau Top Mesmo</h6>
+            <hr>
+            <img src="../img/nescau.png" alt="" id="logo"  >
+            <h4 id="por">R$ 4,50</h4>
+          </div>
+
+          <div class="col-sm" id="item" >
+            <img src="../img/por.png" alt="" id="item2">
+            <h6>Achocolatado Nestlé Nescau Top Mesmo</h6>
+            <hr>
+            <img src="../img/nescau.png" alt="" id="logo"  >
+            <h4 id="por">R$ 4,50</h4>
+          </div>
+
+          <div class="col-sm" id="item" >
+            <img src="../img/por.png" alt="" id="item2">
+            <h6>Achocolatado Nestlé Nescau Top Mesmo</h6>
+            <hr>
+            <img src="../img/nescau.png" alt="" id="logo"  >
+            <h4 id="por">R$ 4,50</h4>
+          </div>
+
+          <div class="col-sm" id="item" >
+            <img src="../img/por.png" alt="" id="item2">
+            <h6>Achocolatado Nestlé Nescau Top Mesmo</h6>
+            <hr>
+            <img src="../img/nescau.png" alt="" id="logo"  >
+            <h4 id="por">R$ 4,50</h4>
+          </div>
+
+          <div class="col-sm" id="item" >
+            <img src="../img/por.png" alt="" id="item2">
+            <h6>Achocolatado Nestlé Nescau Top Mesmo</h6>
+            <hr>
+            <img src="../img/nescau.png" alt="" id="logo"  >
+            <h4 id="por">R$ 4,50</h4>
+          </div>
+
+    </div>
+    <!-- LINHA -->
+    <div class="row">
+
+          <div class="col-sm" id="item" >
+            <img src="../img/por.png" alt="" id="item2">
+            <h6>Achocolatado Nestlé Nescau Top Mesmo</h6>
+            <hr>
+            <img src="../img/nescau.png" alt="" id="logo"  >
+            <h4 id="por">R$ 4,50</h4>
+          </div>
+
+          <div class="col-sm" id="item" >
+            <img src="../img/por.png" alt="" id="item2">
+            <h6>Achocolatado Nestlé Nescau Top Mesmo</h6>
+            <hr>
+            <img src="../img/nescau.png" alt="" id="logo"  >
+            <h4 id="por">R$ 4,50</h4>
+          </div>
+
+          <div class="col-sm" id="item" >
+            <img src="../img/por.png" alt="" id="item2">
+            <h6>Achocolatado Nestlé Nescau Top Mesmo</h6>
+            <hr>
+            <img src="../img/nescau.png" alt="" id="logo"  >
+            <h4 id="por">R$ 4,50</h4>
+          </div>
+
+          <div class="col-sm" id="item" >
+            <img src="../img/por.png" alt="" id="item2">
+            <h6>Achocolatado Nestlé Nescau Top Mesmo</h6>
+            <hr>
+            <img src="../img/nescau.png" alt="" id="logo"  >
+            <h4 id="por">R$ 4,50</h4>
+          </div>
+
+          <div class="col-sm" id="item" >
+            <img src="../img/por.png" alt="" id="item2">
+            <h6>Achocolatado Nestlé Nescau Top Mesmo</h6>
+            <hr>
+            <img src="../img/nescau.png" alt="" id="logo"  >
+            <h4 id="por">R$ 4,50</h4>
+          </div>
+
+    </div>
+    <!-- LINHA -->
+    <div class="row">
+
+          <div class="col-sm" id="item" >
+            <img src="../img/por.png" alt="" id="item2">
+            <h6>Achocolatado Nestlé Nescau Top Mesmo</h6>
+            <hr>
+            <img src="../img/nescau.png" alt="" id="logo"  >
+            <h4 id="por">R$ 4,50</h4>
+          </div>
+
+          <div class="col-sm" id="item" >
+            <img src="../img/por.png" alt="" id="item2">
+            <h6>Achocolatado Nestlé Nescau Top Mesmo</h6>
+            <hr>
+            <img src="../img/nescau.png" alt="" id="logo"  >
+            <h4 id="por">R$ 4,50</h4>
+          </div>
+
+          <div class="col-sm" id="item" >
+            <img src="../img/por.png" alt="" id="item2">
+            <h6>Achocolatado Nestlé Nescau Top Mesmo</h6>
+            <hr>
+            <img src="../img/nescau.png" alt="" id="logo"  >
+            <h4 id="por">R$ 4,50</h4>
+          </div>
+
+          <div class="col-sm" id="item" >
+            <img src="../img/por.png" alt="" id="item2">
+            <h6>Achocolatado Nestlé Nescau Top Mesmo</h6>
+            <hr>
+            <img src="../img/nescau.png" alt="" id="logo"  >
+            <h4 id="por">R$ 4,50</h4>
+          </div>
+
+          <div class="col-sm" id="item" >
+            <img src="../img/por.png" alt="" id="item2">
+            <h6>Achocolatado Nestlé Nescau Top Mesmo</h6>
+            <hr>
+            <img src="../img/nescau.png" alt="" id="logo"  >
+            <h4 id="por">R$ 4,50</h4>
+          </div>
+
+    </div>
+    <!-- LINHA -->
+    <div class="row">
+
+          <div class="col-sm" id="item" >
+            <img src="../img/por.png" alt="" id="item2">
+            <h6>Achocolatado Nestlé Nescau Top Mesmo</h6>
+            <hr>
+            <img src="../img/nescau.png" alt="" id="logo"  >
+            <h4 id="por">R$ 4,50</h4>
+          </div>
+
+          <div class="col-sm" id="item" >
+            <img src="../img/por.png" alt="" id="item2">
+            <h6>Achocolatado Nestlé Nescau Top Mesmo</h6>
+            <hr>
+            <img src="../img/nescau.png" alt="" id="logo"  >
+            <h4 id="por">R$ 4,50</h4>
+          </div>
+
+          <div class="col-sm" id="item" >
+            <img src="../img/por.png" alt="" id="item2">
+            <h6>Achocolatado Nestlé Nescau Top Mesmo</h6>
+            <hr>
+            <img src="../img/nescau.png" alt="" id="logo"  >
+            <h4 id="por">R$ 4,50</h4>
+          </div>
+
+          <div class="col-sm" id="item" >
+            <img src="../img/por.png" alt="" id="item2">
+            <h6>Achocolatado Nestlé Nescau Top Mesmo</h6>
+            <hr>
+            <img src="../img/nescau.png" alt="" id="logo"  >
+            <h4 id="por">R$ 4,50</h4>
+          </div>
+
+          <div class="col-sm" id="item" >
+            <img src="../img/por.png" alt="" id="item2">
+            <h6>Achocolatado Nestlé Nescau Top Mesmo</h6>
+            <hr>
+            <img src="../img/nescau.png" alt="" id="logo"  >
+            <h4 id="por">R$ 4,50</h4>
+          </div>
+
+    </div>
+
+  </div><!-- container -->
+
+</body>
+</html>
