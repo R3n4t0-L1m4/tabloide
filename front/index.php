e6774c5a5c3777ac258da9bad5ce172186315f5c
Renato Lima da Silva <mr.vicdad@gmail.com>
2020-12-09 12:21:39 -0300

    primeira grade de exemplo

--- front/index.php
+++ front/index.php
@@ -16,20 +16,40 @@
     <div class="row">
       <h3>topo - Informações sobre a empresa</h3>
     </div>
-    <div class="row">
-      <h3>corpo - área de tabloide</h3>
-    </div>
+    <div class="row" id="content"></div>
     <div class="row"><h3>rodapé - outras Informações</h3></div>
 
   </div>
 
-  <div class="container" id="content"></div>
   <script src="front/assets/js/jquery-3.4.1.min.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"
     integrity="sha384-q2kxQ16AaE6UbzuKqyBE9/u/KzioAlnx2maXQHiDX9d4/zp8Ok3f+M7DPm+Ib6IU" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.min.js"
     integrity="sha384-pQQkAEnwaBkjpqZ8RU1fF1AKtTcHJwFl3pblpTlHXybJjHpMYo79HY3hIi4NKxyj" crossorigin="anonymous"></script>
 
+  <script>
+    function getItems(){
+      $('#content').html('');
+      $.get('/api/produto.php', produtos => {
+        $.each(produtos, (chave, produto) => {
+          let quadro = $(`
+            <div class="col-sm">
+
+              <h6 id="tp">${produto.titulo}</h6>
+              <hr>
+              <img src="${produto.img}" alt="">
+              <h4 id="por">R$ ${produto.depreco}</h4>
+            </div>
+          `);
+          $('#content').append(quadro);
+        });
+      });
+    }
+
+    $(document).ready(()=>{
+      getItems();
+    });
+  </script>
 
 
 </body>
